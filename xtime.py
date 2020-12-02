de8af5bc58bb486abd9aae9cb8a07eb4df0d4bdf
Mike <m400@protonmail.com>
2020-12-01 22:42:28 -0500

    Updated color code and text.

--- xtime.py
+++ xtime.py
@@ -14,47 +14,47 @@ def main():
 
 def sysos():
   output = str.splitlines(os.fsdecode(subprocess.check_output(['lsb_release', '-idrc'])))
-  print('\n' + esc('1;34') + ' Operating System Distribution' + esc(0))
+  print('\n' + esc('1;93') + ' Operating System Description' + esc(0))
   for line in output:
     print(' ' + line)
   print()
 
 def systime():
   time = datetime.now()
-  print(esc('1;34') + ' System Time: '+ esc(0)  + time.strftime('%I:%M %p -> %b %d %Y'))
+  print(esc('1;93') + ' System Time: '+ esc(0)  + time.strftime('%I:%M %p -> %b %d %Y'))
 
 def sysuptime():
   output = os.fsdecode(subprocess.check_output(['uptime', '-p']))
   sysuptime = re.search(r"up\s(.*)" , output).group(1)
-  print(esc('1;34') + ' System\'s been up for:' + esc(0), sysuptime)
+  print(esc('1;93') + ' System\'s been up for:' + esc(0), sysuptime)
 
 def sessions():
   output = os.fsdecode(subprocess.check_output('uptime'))
   out = re.search(r"(\d+)\s+users", output).group(1)
-  print(esc('1;34') + ' Number of active user sessions: ' + esc(0), out + ' user sessions')
+  print(esc('1;93') + ' Number of active user sessions: ' + esc(0), out + ' user sessions')
 
 def loadavg():
   output = os.fsdecode(subprocess.check_output('uptime'))
   load = re.search(r"(load average):(.*)", output).group(2)
-  print(esc('1;34') + ' Load average for last 1, 5 and 15 minutes:' + esc(0), load + '\n')
+  print(esc('1;93') + ' Load average for last 1, 5 and 15 minutes:' + esc(0), load + '\n')
 
 def users():
   output = str.splitlines(os.fsdecode(subprocess.check_output(['w', '-h'])))
-  print(esc('1;34') + " List of logged in users and what they're doing." + esc(0))
+  print(esc('1;93') + " List of logged in users and what they're doing" + esc(0))
   for line in output:
     print(" " + line)
   print('')
 
 def memory():
   output = str.splitlines(os.fsdecode(subprocess.check_output(['free', '-ht'])))
-  print(esc('1;34') + " Display free and used memory. " + esc(0))
+  print(esc('1;93') + " Free and used system memory " + esc(0))
   for line in output:
     print(" " + line)
   print('')
 
 def storage():
   output = str.splitlines(os.fsdecode(subprocess.check_output(['lsblk', '-fm', '-o' 'NAME,' 'FSTYPE,' 'FSAVAIL,' 'FSUSE%,' 'MOUNTPOINT,' 'SIZE,' 'OWNER,' 'GROUP,' 'MODE', '-e 7'])))
-  print(esc('1;34') + " List of drives, partitions, and details" + esc(0))
+  print(esc('1;93') + " List of drives, partitions, and details" + esc(0))
   for line in output:
     print(" " + line)
   print('')
